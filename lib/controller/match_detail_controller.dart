c401329493a5a92b2d8123d4a6bdd8ce6f3ba6e4
HWSEO-AKINOKAMI-53183 <hwseo-akinokami-53183@admindeMac-mini.local>
2024-08-31 16:30:42 +0800

    a

--- lib/controller/match_detail_controller.dart
+++ lib/controller/match_detail_controller.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:tennis_live_score/models/match_detail.dart';
+import 'package:tennis_live_score/models/point_by_point.dart';
 import 'package:tennis_live_score/models/stats.dart';
 
 import '../services/api_repo.dart';
@@ -8,14 +9,18 @@ import '../utils/constants.dart';
 
 class MatchDetailController extends GetxController {
   final isLoading = false.obs;
-  Rx<Stats> stats = Stats().obs;
+
   RxInt selectedIndex = 0.obs;
+
   Rx<MatchDetail> matchDetail = MatchDetail().obs;
+  Rx<Stats> stats = Stats().obs;
+  Rx<PointByPoint> pointByPoint = PointByPoint().obs;
   final gameId = 0.obs;
 
   @override
   void onInit() {
     gameId.value = Get.arguments['gameId'];
+    getMatchDetail();
     super.onInit();
   }
 
@@ -41,6 +46,19 @@ class MatchDetailController extends GetxController {
     try {
       final result = await ApiRepo().getStats(gameId.value);
       stats.value = result;
+    } catch (e) {
+      constants.showSnackBar(
+          title: 'Error', msg: e.toString(), textColor: Colors.red);
+    } finally {
+      isLoading.value = false;
+    }
+  }
+
+  Future<void> getPointByPoint() async {
+    isLoading.value = true;
+    try {
+      final result = await ApiRepo().getPointByPoint(gameId.value);
+      pointByPoint.value = result;
     } catch (e) {
       constants.showSnackBar(
           title: 'Error', msg: e.toString(), textColor: Colors.red);
